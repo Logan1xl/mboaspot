05dccbd48ff713f8252c13ab9d9c098fa0eb66a5
carmelle2 <carmelguillene93@gmail.com>
2025-12-01 15:15:09 +0100

    creation du DTO UtilisateurDTO

---
+++ backend/src/main/java/com/example/backend/dto/UtilisateurDTO.java
@@ -0,0 +1,13 @@
+package com.example.backend.dto;
+
+public class UtilisateurDTO {
+    private Long id;
+    private String email;
+    private String motDePasse;
+    private String prenom;
+    private String nom;
+    private String numeroTelephone;
+    private String photoProfil;
+    private Boolean estActif;
+    private String role;
+}
