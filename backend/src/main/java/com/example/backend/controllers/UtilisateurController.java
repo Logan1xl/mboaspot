9dab22463d7b75c2016f4b3f9a25d31bb43e055e
carmelle2 <carmelguillene93@gmail.com>
2025-12-03 12:05:05 +0100

    realisation du crud des utilisateurs en creant le mapper,repository,le service et son interface et le controller

---
+++ backend/src/main/java/com/example/backend/controllers/UtilisateurController.java
@@ -0,0 +1,69 @@
+package com.example.backend.controllers;
+
+import com.example.backend.dto.UtilisateurDTO;
+import com.example.backend.services.implementation.UtilisateurService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UtilisateurController {
+
+
+    private final UtilisateurService utilisateurService;
+    private UtilisateurService Utilisateurservice;
+
+    public UtilisateurController(UtilisateurService utilisateurService) {
+        this.utilisateurService = utilisateurService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllUtilisateurs() {
+        return ResponseEntity.ok(Utilisateurservice.getAllUtilisateurs());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUtilisateurById(@PathVariable Long id) {
+        try {
+            UtilisateurDTO utilisateurDTO  = utilisateurService.getUtilisateurById(id);
+            return new ResponseEntity<>(utilisateurDTO, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUtilisateur(@RequestBody UtilisateurDTO utilisateurDTO) {
+        try {
+            UtilisateurDTO createdUtilisateur = utilisateurService.createUtilisateur(utilisateurDTO);
+            return new ResponseEntity<>(createdUtilisateur, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUtilisateur(@PathVariable Long id, @RequestBody UtilisateurDTO utilisateurDTO) {
+        try {
+            UtilisateurDTO updatedUtilisateur = utilisateurService.updateUtilisateur(id, utilisateurDTO);
+            return new ResponseEntity<>(updatedUtilisateur, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUtilisateur(@PathVariable Long id) {
+        try {
+            utilisateurService.deleteUtilisateur(id);
+            return new ResponseEntity<>("Utilisateur supprimé avec succès", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+}
---
+++ backend/src/main/java/com/example/backend/services/implementation/UtilisateurService.java
@@ -0,0 +1,86 @@
+package com.example.backend.services.implementation;
+
+
+
+import com.example.backend.dto.UtilisateurDTO;
+import com.example.backend.entities.Utilisateur;
+import com.example.backend.mappers.UtilisateurMapper;
+import com.example.backend.repositories.UtilisateurRepos;
+import com.example.backend.services.interfaces.UtilisateurInterface;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UtilisateurService implements UtilisateurInterface {
+
+
+    private UtilisateurRepos repository;
+    private PasswordEncoder passwordEncoder;
+    private UtilisateurMapper utilisateurMapper;
+
+    public UtilisateurService(UtilisateurRepos repository, PasswordEncoder passwordEncoder, UtilisateurMapper utilisateurMapper) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+        this.utilisateurMapper = utilisateurMapper;
+    }
+
+    public List<UtilisateurDTO> getAllUtilisateurs() {
+        return repository.findAll()
+                .stream()
+                .map(utilisateurMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public UtilisateurDTO getUtilisateurById(Long id) {
+       try{
+           Utilisateur utilisateur = repository.findById(id).get();
+              return utilisateurMapper.toDTO(utilisateur);
+       }catch(Exception e){
+              throw new RuntimeException("Utilisateur non trouvé avec l'id: " + id);
+       }
+    }
+
+    public UtilisateurDTO createUtilisateur(UtilisateurDTO utilisateurDTO) {
+        if (repository.existsByEmail(utilisateurDTO.getEmail())) {
+            throw new RuntimeException("Email déjà utilisé");
+        }else{
+            try{
+                return utilisateurMapper.toDTO(repository.save(utilisateurMapper.toEntity(utilisateurDTO)));
+            }catch(Exception e){
+                throw new RuntimeException("Erreur lors de la création de l'utilisateur: " + e.getMessage());
+            }
+
+        }
+
+    }
+
+    public UtilisateurDTO updateUtilisateur(Long id, UtilisateurDTO utilisateurDTO) {
+        try{
+            Utilisateur existingUtilisateur = repository.findById(id).get();
+            if (existingUtilisateur == null) {
+                throw new RuntimeException("Utilisateur non trouvé avec l'id: " + id);
+            }else {
+                existingUtilisateur.setNom(utilisateurDTO.getNom());
+                existingUtilisateur.setPrenom(utilisateurDTO.getPrenom());
+                existingUtilisateur.setEmail(utilisateurDTO.getEmail());
+                existingUtilisateur.setRole(utilisateurDTO.getRole());
+                repository.save(existingUtilisateur);
+                return utilisateurMapper.toDTO(existingUtilisateur);
+            }
+
+        }catch (Exception e){
+            throw new RuntimeException("Erreur lors de la mise à jour de l'utilisateur: " + e.getMessage());
+        }
+    }
+
+    public void deleteUtilisateur(Long id) {
+        if (!repository.existsById(id)) {
+            throw new RuntimeException("Utilisateur non trouvé avec l'id: " + id);
+        }
+        repository.deleteById(id);
+    }
+
+}
